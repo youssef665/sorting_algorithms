959520dc972b7c7d4ba32e2bd7328117b514aa73
youssef665 <yi559668@gmail.com>
2023-10-01 05:08:05 -0700

    45

---
+++ 100-shell_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+*swap - the positions of two elements
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+
+void swap(int *array, int item1, int item2)
+{
+
+	int t;
+
+	t = array[item1];
+	array[item1] = array[item2];
+
+	array[item2] = t;
+}
+
+/**
+ * shell_sort - function that sorts an array 
+ * order using the Shell sort algorithe
+ * @size: size
+ * @array: list 
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, index = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+
+		gap /= 3;
+	}
+}
